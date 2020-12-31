288954451833b2fe34bb8bd530b3187917bb5f98
Karsten Lüdtke <karsten.luedtke@posteo.de>
2020-12-31 17:19:38 +0100

    added event listeners

--- lib/Subscription.d.ts
+++ lib/Subscription.d.ts
@@ -2,20 +2,34 @@ import {EventEmitter} from 'events'
 
 import {UA} from './UA'
 import {Transport} from './Transport'
-import {ExtraHeaders} from './RTCSession'
+import {ExtraHeaders, Originator, OutgoingListener, IncomingListener, IncomingInfoListener, EndedListener} from './RTCSession'
+
+export interface SubscriptionEventMap {
+  accepted: OutgoingListener;
+  confirmed: IncomingInfoListener;
+  notify: IncomingInfoListener;
+  subscriptionExpiring: IncomingListener;
+  ended: EndListener;
+}
 
 export interface SubscribeOptions extends ExtraHeaders {
   expires?: number;
   refresh?: boolean;
-  //eventHandlers?: Partial<MessageEventMap>;
+  eventHandlers?: Partial<SubscriptionEventMap>;
 }
 
 export class Subscription extends EventEmitter {
   constructor(ua: UA, target: string, event: string, transport: Transport);
+
+  get id(): string;
+
+  get state(): string;
 
   setExtraHeaders(extraHeaders: string[]): void;
 
   subscribe(options?: SubscribeOptions): void;
 
   unsubscribe(): void;
+
+  on<T extends keyof SubscriptionEventMap>(type: T, listener: SubscriptionEventMap[T]): this;
 }
