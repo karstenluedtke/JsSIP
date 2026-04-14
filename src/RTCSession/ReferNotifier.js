13f8ee2db8b219d7bb70cdeb24c5984c574482b7
Karsten Lüdtke <karsten.luedtke@posteo.de>
2026-04-14 08:54:59 +0200

    being transferred (unattended) with transferer disconnecting prematurely

--- src/RTCSession/ReferNotifier.js
+++ src/RTCSession/ReferNotifier.js
@@ -27,6 +27,11 @@ module.exports = class ReferNotifier {
 			return;
 		}
 
+		if (this._session.status === this._session.C.STATUS_TERMINATED) {
+			logger.debug('notify(): session already terminated');
+			return;
+		}
+
 		reason = reason || JsSIP_C.REASON_PHRASE[code] || '';
 
 		let state;
